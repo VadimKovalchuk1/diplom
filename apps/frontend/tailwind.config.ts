3afc74ddf8e22d90b490ed11dd82d6006081701e
VadimKovalchuk1 <71716032+VadimKovalchuk1@users.noreply.github.com>
2026-06-06 22:43:55 +0300

    Create tailwind.config.ts

---
+++ apps/frontend/tailwind.config.ts
@@ -0,0 +1,3 @@
+import type { Config } from 'tailwindcss';
+const config: Config = { content: ['./src/**/*.{ts,tsx}'], theme: { extend: { colors: { federal: '#123c69' } } }, plugins: [] };
+export default config;
